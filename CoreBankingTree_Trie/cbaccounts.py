ad7dbaad329dd83b5f31661424b43258bb4626d6
hendrasoewarno <hendrasoewarno@gmail.com>
2024-06-15 13:38:03 +0700

    Update cbaccounts.py

--- CoreBankingTree_Trie/cbaccounts.py
+++ CoreBankingTree_Trie/cbaccounts.py
@@ -8,19 +8,20 @@ class Account:
         self.noac=noac
         self.nama=nama.upper()
         self.nik=nik
-        self.pin="123456"        
-        self.balance=0
+        self.__pin="123456"        
+        self.__balance=0
+        #masing-masing Account punya tabel transaksi masing2
         self.transaction = LinkedListTransaction()
 
     #tambahkan pemeriksaan amount > 0
     def deposit(self, cabang, amount):    
-        self.balance+=amount
+        self.__balance+=amount
         self.transaction.newTransaction(cabang, "12", amount)
         
     #tambahkan pemeriksaan amount > 0
     #tambahkan pemeriksaan setelah penarikan tidak boleh < dari minDeposit
     def withdraw(self, cabang, amount):
-        self.balance-=amount
+        self.__balance-=amount
         self.transaction.newTransaction(cabang, "21", amount)
 
     def show(self):
@@ -28,134 +29,48 @@ class Account:
         print("No.AC:", self.noac)
         print("Nama:", self.nama)
         print("NIK:", self.nik)
-        print("Amount:", f'{self.balance:,}')
+        print("Amount:", f'{self.__balance:,}')
 
     def print(self):
         print(self.noac.ljust(14), self.nama.ljust(50), self.nik.ljust(16))
 
-#kita tambahkan TrieNode
-class TrieNode:
+class ArrayAccount:
     def __init__(self):
-        self.childNode = [None] * 26
-        self.array = []
-
-#kita tambahkan Trie untuk pencarian akun berdasarkan nama
-class Trie:
-    def __init__(self):
-        self.root = TrieNode()
-
-    # Function to insert a key into the Trie
-    def insert(self, account):
-        currentNode = self.root
-        for char in account.nama:
-            index = ord(char) - ord('A')
-            if not currentNode.childNode[index]:
-                currentNode.childNode[index] = TrieNode()
-            currentNode = currentNode.childNode[index]
-        currentNode.array.append(account)
-
-    # Function to search for a nama in the Trie
-    def search(self, nama):
-        currentNode = self.root
-        for char in nama:
-            index = ord(char) - ord('A')
-            if not currentNode.childNode[index]:
-                return False
-            currentNode = currentNode.childNode[index]
-        return currentNode.array        
-
-class AccountNode:
-    def __init__(self, account):
-        self.account=account
-        self.left = None
-        self.right = None
-
-class BinarySearchTreeAccount:
-    def __init__(self):
-        self.root=None
-        #buat trie untuk menyimpan account berdasarkan nama
-        self.trie=Trie() 
+        self.array =[]
         self.count=0
         
-    #non recursive approach
     def newAccount(self,cabang, nik, nama):
         self.count+=1
         noac=cabang + str(self.count).rjust(4,"0")
         account=Account(noac, nama, nik)
-        newNode = AccountNode(account)
-        
-        if not self.root:
-            self.root = newNode
-        else:
-            lastPosition=self.root
-            while lastPosition:
-                if account.noac < lastPosition.account.noac:
-                    if not lastPosition.left:
-                        lastPosition.left = newNode
-                        break
-                    else:
-                        lastPosition = lastPosition.left
-                else:
-                    if not lastPosition.right:
-                        lastPosition.right = newNode
-                        break
-                    else:
-                        lastPosition = lastPosition.right
-                        
-        #tambahkan account ke Trie
-        self.trie.insert(account)
-                        
+        self.array.append(account)        
         return account
-
-    #recursive approach
-    def findTree(self, start, noac):
-        if start:
-            start.account.print()
-            if noac == start.account.noac:
-                return start.account
-            elif noac < start.account.noac:
-                return self.findTree(start.left, noac)
-            else:
-                return self.findTree(start.right, noac)
-        else:
-            return None
             
+    #linear search
     def find(self, noac):
-        return self.findTree(self.root, noac)        
-        
-    #recursive approach
-    def listTree(self, start):
-        if start:            
-            if start.left:
-                self.listTree(start.left)
-                
-            start.account.print()    
-            
-            if start.right:
-                self.listTree(start.right)
+        lastPosition = 0
+        while lastPosition < self.count:
+            account=self.array[lastPosition]
+            if account.noac==noac:
+                return account
+            lastPosition+=1
+        return None
         
     def list(self):
-        lastPosition=self.root
+        lastPosition=0
         print("\n\n>>DAFTAR ACCOUNT\n\n")
         print("-"*(80+3))
         print("No.AC".ljust(14),"Nama".ljust(50),"NIK".ljust(16))
         print("-"*(80+3))
-        self.listTree(self.root)
-        
-    def listByNama(self, nama):
-        print("\n\n>>DAFTAR ACCOUNT\n\n")
-        print("-"*(80+3))
-        print("No.AC".ljust(14),"Nama".ljust(50),"NIK".ljust(16))
-        print("-"*(80+3))
-        array=self.trie.search(nama)
-        for account in array:
+        while lastPosition < self.count:
+            account=self.array[lastPosition]
             account.print()
-
+            lastPosition+=1
 
 #unit test
 #12=Sumatera Utara, 71=Kota Medan, 11=Medan Johor, 01=Cabang 01, 01=Tabungan
 #cabang="1271110101"
-#savingAccount=BinarySearchTreeAccount()
+#savingAccount=LinkedListAccount()
 #account1=savingAccount.newAccount(cabang, "1271181906730004", "Hendra Soewarno")
 #assert account1.nik=="1271181906730004", "Error NIK 1271181906730004"
 #account2=savingAccount.newAccount(cabang, "1271181906730005", "Susan")
@@ -165,12 +80,12 @@ class BinarySearchTreeAccount:
 #myAccount = savingAccount.find(account1.noac)
 #myAccount.show()
 #myAccount.deposit(cabang, 100000)
-#assert myAccount.balance==100000, "Error Deposit, new balance 100000 not match"
+#assert myAccount.__balance==100000, "Error Deposit, new balance 100000 not match"
 #myAccount.show()
 #myAccount.deposit(cabang, 100000)
-#assert myAccount.balance==100000, "Error Deposit, new balance 200000 not match"
+#assert myAccount.__balance==100000, "Error Deposit, new balance 200000 not match"
 #myAccount.show()
 #myAccount.withdraw(cabang, 100000)
-#assert myAccount.balance==100000, "Error Deposit, new balance 100000 not match"
+#assert myAccount.__balance==100000, "Error Deposit, new balance 100000 not match"
 #myAccount.show()
 #myAccount.transaction.list()
